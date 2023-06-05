4c6f7b58e2c1bfd5a03199c7ca5c99cb1318ea13
SamOJackson <127228299+SamOJackson@users.noreply.github.com>
2023-06-05 13:44:01 -0400

    assignment1Render
    render upload

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Portfolio: Sam Jackson' });
+});
+
+router.get('about', (req, res, next) =>{
+  res.render('about', { title: 'About Me' });
+});
+
+router.get('projects', (req, res, next) =>{
+  res.render('projects', { title: 'My Projects' });
+});
+
+router.get('contact', (req, res, next) =>{
+  res.render('contact', { title: 'Contact Me' });
+});
+
+module.exports = router;
